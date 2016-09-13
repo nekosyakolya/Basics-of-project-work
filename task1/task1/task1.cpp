7b1edf56318c7e78e3c442daa51d6c83d575deeb
nekosyakolya <greirex@mail.ru>
2016-09-13 14:50:50 +0300

    вынесение g в директиву.переименовние флага

--- task1/task1/task1.cpp
+++ task1/task1/task1.cpp
@@ -7,22 +7,24 @@
 #include <cmath>
 #include <iomanip>
 #include <cfloat>
+#define G 9.8f
 
 // This program takes max jump height from input and prints
 // jump height for every time point with step 0.1 seconds.
 // Program should print all time points when height is min and max.
-//
 // TODO: Fix all warnings on high warning level (/W4, -Wall -Wextra).
 // TODO: Rename variables and split to several functions,
 // see also https://ps-group.github.io/sfml/coding_conventions.html
 // TODO: fix negative height values, fix heigh values higher than max height.
-void PrintCurrentState(const float &G, const float &time, const float &v0);
-void DisplayStates(const float &G, const float &timeReachMaxHeight);
+
+using namespace std;
+void PrintCurrentState(const float &time, const float &v0);
+void DisplayStates(const float &timeReachMaxHeight);
 int GetHeightJump();
 void Pause();
 
-using namespace std;
-void PrintCurrentState(const float &G, const float &time, const float &v0)
+
+void PrintCurrentState(const float &time, const float &v0)
 {
 	double currHeight = v0 * time - 0.5 * G * time * time;
 	if (currHeight >= 0)
@@ -31,19 +33,19 @@ void PrintCurrentState(const float &G, const float &time, const float &v0)
 	}
 }
 
-void DisplayStates(const float &G, const float &timeReachMaxHeight)
+void DisplayStates(const float &timeReachMaxHeight)
 {
 	cout << "Time point when height is at maximum = " << fixed << setprecision(6) << timeReachMaxHeight << "\n";
-	bool flag = false;
+	bool printMaxState = false;
 	for (float currTime = 0; currTime < timeReachMaxHeight * 2; currTime += 0.1f)
 	{
 		float v0 = G * timeReachMaxHeight;
-		if (currTime > timeReachMaxHeight && !flag)
+		if (currTime > timeReachMaxHeight && !printMaxState)
 		{
-			flag = true;
-			PrintCurrentState(G, timeReachMaxHeight, v0);
+			printMaxState = true;
+			PrintCurrentState(timeReachMaxHeight, v0);
 		}
-		PrintCurrentState(G, currTime, v0);
+		PrintCurrentState(currTime, v0);
 	}
 }
 
@@ -77,15 +79,14 @@ void Pause()
 
 int main()
 {
-	const float G = 9.8f;
 	float timeReachMaxHeight;
 	int heightJump = GetHeightJump();
 	
 	timeReachMaxHeight = sqrt(heightJump * 2 / G);
-	DisplayStates(G, timeReachMaxHeight);
+	DisplayStates(timeReachMaxHeight);
 	
 	float v0 = G * timeReachMaxHeight;
-	PrintCurrentState(G, timeReachMaxHeight * 2, v0);
+	PrintCurrentState(timeReachMaxHeight * 2, v0);
 	Pause();
 	return 0;
 }
