759c2491e088f7bbebbfb5fb006eff81ac4a7ebd
nekosyakolya <greirex@mail.ru>
2016-10-11 15:45:03 +0300

    замена unsigned int

--- sfmltest/sfmltest/consts.h
+++ sfmltest/sfmltest/consts.h
@@ -1,9 +1,9 @@
 #pragma once
 
-static const unsigned int SCREEN_WIDTH = 1200;
-static const unsigned int SCREEN_HEIGHT = 700;
+static const unsigned SCREEN_WIDTH = 1200;
+static const unsigned SCREEN_HEIGHT = 700;
 static const float CLOCK_CIRCLE_SIZE = 250;
 static const float CLOCK_CIRCLE_THICKNESS = 2;
-static const unsigned int NUMBER_DOTS = 60;
-static const unsigned int NUMBER_DIGITS = 12;
-static const unsigned int LEVEL_ANTIALIASING = 8;
+static const unsigned NUMBER_DOTS = 60;
+static const unsigned NUMBER_DIGITS = 12;
+static const unsigned LEVEL_ANTIALIASING = 8;
