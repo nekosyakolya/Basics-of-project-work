b7298a300af984f72eb0c20a6986655f7acac428
nekosyakolya <greirex@mail.ru>
2017-01-23 05:17:47 +0300

    добавлены враги

--- Game/newGame/Game.cpp
+++ Game/newGame/Game.cpp
@@ -27,16 +27,34 @@ void CGame::Initialisation()
 		m_bonuses.push_back(std::shared_ptr<CDonut>(new CDonut(sf::Vector2f(currBonus.rect.left, currBonus.rect.top))));
 	}
 
+
+
+	std::vector<Object> enemyStartPos = m_level.GetObjects("enemy");
+	sf::Clock clock;
+	for (auto currEnemy : enemyStartPos)
+	{
+		float timeSpeed = static_cast<float>(clock.getElapsedTime().asMicroseconds());
+		clock.restart();
+		timeSpeed = timeSpeed / 10000000;
+		m_enemies.push_back(std::shared_ptr<CEnemy>(new CEnemy(sf::Vector2f(currEnemy.rect.left, currEnemy.rect.top), timeSpeed)));
+	}
+
+
+	for (auto enemy : m_enemies)
+	{
+		enemy->Init(m_level);
+	}
+
 }
 
 
 
-Level CGame::GetLevel()
+Level CGame::GetLevel()const
 {
 	return m_level;
 }
 
-CPlayer CGame::GetPlayer()
+CPlayer CGame::GetPlayer()const
 {
 	return m_player;
 }
@@ -48,11 +66,8 @@ void CGame::UpdatePlayer(float time)
 
 }
 
-void CGame::Update(float time)
+void CGame::CheckCollisionWithDonuts()
 {
-	UpdatePlayer(time);
-	
-
 	for (size_t i = 0; i < m_bonuses.size(); ++i)
 	{
 		if (m_player.GetRect().intersects(m_bonuses[i]->GetSprite().getGlobalBounds()))
@@ -63,6 +78,64 @@ void CGame::Update(float time)
 			m_player.UpdateTotal(5);
 		}
 	}
+}
+
+void CGame::DisplayDonuts(sf::RenderWindow & window)const
+{
+	for (auto bonus : m_bonuses)
+	{
+		window.draw(bonus->GetSprite());
+	}
+}
+
+void CGame::Update(float time)
+{
+	UpdatePlayer(time);
+	
+
+	for (auto enemy : m_enemies)
+	{
+		enemy->Update(time);
+
+		if (m_player.GetRect().intersects(enemy->GetHero().getGlobalBounds()))
+		{
+			if (m_player.GetOffset().x < 0)
+			{
+				m_player.UpdatePosition(enemy->GetPosition().x + 40);
+				enemy->UpdatePosition(-0.1f * time);
+			}
+			if (m_player.GetOffset().x > 0)
+			{
+				m_player.UpdatePosition(enemy->GetPosition().x - 40);
+				enemy->UpdatePosition(0.1f * time);
+			}
+
+
+			if (m_player.GetOffset().x == 0)
+			{
+				if (!enemy->Check(0.1f * time))
+				{
+					enemy->Check(-0.1f * time);
+				}
+
+			}
+		}
+
+
+		for (size_t i = 0; i < m_bonuses.size(); ++i)
+		{
+			if (enemy->GetRect().intersects(m_bonuses[i]->GetSprite().getGlobalBounds()))
+			{
+				m_bonuses.erase(m_bonuses.begin() + i);
+			}
+		}
+
+		enemy->SetPosition();
+	}
+
+
+
+	CheckCollisionWithDonuts();
 
 
 	m_player.SetPosition();
@@ -75,19 +148,43 @@ void CGame::Update(float time)
 
 }
 
-void CGame::Display(sf::RenderWindow & window)
+void CGame::Display(sf::RenderWindow & window)const
 {
 	window.setView(m_view);
 
 	window.clear(sf::Color::White);
 	GetLevel().Draw(window);
 
-	for (auto bonus : m_bonuses)
+	DisplayDonuts(window);
+
+
+	for (auto enemy : m_enemies)
 	{
-		window.draw(bonus->GetSprite());
+		window.draw(enemy->GetHero());
+
+		if (enemy->IsJump())
+		{
+			window.draw(enemy->GetRectProtection());
+		}
 	}
 
+
+
 	window.draw(GetPlayer().GetHero());
+
+	if (GetPlayer().IsJump())
+	{
+		window.draw(m_player.GetSpriteProtection());
+	}
+
+	if (m_player.IsFreeze())
+	{
+		window.draw(m_player.GetSpriteFreeze());
+	}
+
+
+
+
 	window.display();
 }
 
--- Game/newGame/Game.h
+++ Game/newGame/Game.h
@@ -3,6 +3,7 @@
 #include "level.h"
 #include "Player.h"
 #include "Donut.h"
+#include "Enemy.h"
 
 #include <memory>
 
@@ -13,21 +14,27 @@ public:
 	CGame();
 	void Initialisation();
 
-	Level GetLevel();
-	CPlayer GetPlayer();
+	Level GetLevel()const;
+	CPlayer GetPlayer()const;
 
 
 	void Update(float);
 
-	void Display(sf::RenderWindow &);
+	void Display(sf::RenderWindow &)const;
 
 	~CGame();
 private:
 	Level m_level;
 	CPlayer m_player;
 	sf::View m_view;
-	void UpdatePlayer(float);
+	
 	std::vector<std::shared_ptr<CDonut>> m_bonuses;
+	std::vector<std::shared_ptr<CEnemy>> m_enemies;
+
+	void CheckCollisionWithDonuts();
+	void UpdatePlayer(float);
+
+	void DisplayDonuts(sf::RenderWindow &)const;
 
 };
 
